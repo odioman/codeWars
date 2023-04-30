6c767e33959d1af58c4fee339984b24b4a8d62e2
Thomas Koshy <ttkoshy@gmail.com>
2023-04-29 20:53:45 -0400

    newsest kata added

--- main.js
+++ main.js
@@ -134,3 +134,31 @@ function betweenExtremes(numbers) {
         return false;
       }
     }
+
+/* Building blocks*/
+class Block{
+    constructor(data){
+      this.width = data[0];
+      this.length = data[1];
+      this.height = data[2];
+    }
+    
+    getWidth() {
+      return this.width;  
+    }
+    
+    getLength() {
+      return this.length;
+    }
+    
+    getHeight() {
+      return this.height;
+    }
+    
+    getVolume() {
+      return this.width * this.length * this.height;
+    }
+    
+    getSurfaceArea() {
+      return 2 * ((this.width * this.length) + (this.height * this.length) + (this.height * this.width))
+    }
