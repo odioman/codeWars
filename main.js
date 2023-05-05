92a94dbdc4a0c5a30bf1a773d1cc2d43b8abee89
Thomas Koshy <ttkoshy@gmail.com>
2023-05-05 18:07:43 -0400

    newest kata added

--- main.js
+++ main.js
@@ -210,3 +210,12 @@ function makeNegative(num) {
       return -num
     } 
   }
+
+/* 'this' is a problem */
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.name = first + ' ' + last;
+}
+
+let n = new NameMe('John', 'Doe')
