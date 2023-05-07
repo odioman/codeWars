b414e1d00f345dcd960269c3c4afa1cad7c4e49f
Thomas Koshy <ttkoshy@gmail.com>
2023-05-07 14:43:11 -0400

    newest kata added

--- main.js
+++ main.js
@@ -261,4 +261,16 @@ function summation(num) {
     const reduceNum = arrayNum.reduce((a,b) => a + b);
     return reduceNum
   }
-  
+
+/* Rock Paper Scissors */
+const rps = (p1, p2) => {
+    if (p1 === 'rock' && p2 === 'scissors' || p1 === 'scissors' && p2 === 
+       'paper'|| p1 === 'paper' && p2 === 'rock') {
+      return 'Player 1 won!'
+    } else if (p1 === 'rock' && p2 === 'rock' || p1 === 'scissors' && p2 === 
+              'scissors' || p1 === 'paper' && p2 === 'paper') {
+      return "Draw!"
+    } else {
+      return "Player 2 won!"
+    }
+}
